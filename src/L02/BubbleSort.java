d4a209b4ddb3a8460ed436ad3684d2fbaeb8fa77
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-10-09 18:55:43 +0300

    new

---
+++ src/L02/BubbleSort.java
@@ -0,0 +1,37 @@
+package L02;
+
+abstract class BubbleSort {
+    //O(n^2)
+    public static void bubbleSort(int[] a) {
+        int counter = 0;
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++) {
+                counter++;
+                if (a[j] > a[j + 1])
+                    swap(a,j, j + 1);
+            }
+        System.out.println(counter);
+    }
+
+    //O(n^2)
+    public static void BBsort(int[] a) {
+        int counter = 0;
+        int n = a.length;
+        for (int i = 0; i < n ; i++)
+            for (int j = 0; j < n -1 ; j++) {
+                counter++;
+                if (a[j] > a[j + 1])
+                    swap(a,j, j + 1);
+            }
+        System.out.println(counter);
+    }
+
+    //O(1)
+    private static void swap(int[] a,int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+}
