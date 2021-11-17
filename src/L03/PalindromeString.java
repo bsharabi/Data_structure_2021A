202bcd2e7914491cc3c8c0f8e16f0d56b9b118a3
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-17 12:27:41 +0200

    new Lesson 03

---
+++ src/L03/PalindromeString.java
@@ -0,0 +1,17 @@
+package L03;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "abcba";
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() == 0 || str.length() == 1)
+            return true;
+        int end = str.length() - 1;
+        if (str.charAt(0) == str.charAt(end))
+            return isPalindrome(str.substring(1, end));
+        return false;
+    }
+}
