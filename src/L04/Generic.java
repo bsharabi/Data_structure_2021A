48b5e75ac73cccd334df22d525a6c6ccbecda59c
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-23 22:44:18 +0200

    new les

---
+++ src/L04/Generic.java
@@ -0,0 +1,15 @@
+package L04;
+
+public class Generic {
+
+    static <T> void IsEq(T n1, T n2) {
+        System.out.println("n1 = " + n1 + " , n2 = " + n2);
+    }
+
+    public static void main(String[] args) {
+        IsEq(2, 2);
+        IsEq("bob", "bob");
+        IsEq(true, true);
+
+    }
+}
